4c189ac07f55adf5fc477b943b2c642c8d4b802d
unknown <eric.eduardo99@gmail.com>
2021-10-21 09:32:59 -0300

    tetse 2

---
+++ eric-teste-minha-maquina2/src/main/java/com/br/erictesteminhamaquina2/EricTesteMinhaMaquina2Application.java
@@ -0,0 +1,13 @@
+package com.br.erictesteminhamaquina2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricTesteMinhaMaquina2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricTesteMinhaMaquina2Application.class, args);
+	}
+
+}
---
+++ eric-teste-minha-maquina2/src/test/java/com/br/erictesteminhamaquina2/EricTesteMinhaMaquina2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.erictesteminhamaquina2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricTesteMinhaMaquina2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
