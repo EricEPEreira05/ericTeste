d5c3f57a1ff6d7afb43503a5391568f1c148426a
unknown <eric.eduardo99@gmail.com>
2021-10-21 08:53:48 -0300

    test eric minha maquina

---
+++ eric-teste-minha-maquina/src/main/java/com/br/erictesteminhamaquina/EricTesteMinhaMaquinaApplication.java
@@ -0,0 +1,13 @@
+package com.br.erictesteminhamaquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricTesteMinhaMaquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricTesteMinhaMaquinaApplication.class, args);
+	}
+
+}
